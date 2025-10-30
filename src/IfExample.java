71e1f16cb9a5ac32a53d7fe7533158b08ebe932b
samndambuki <sam.ndambuki08@gmail.com>
2025-10-30 21:42:47 +0300

    final commit

---
+++ src/IfExample.java
@@ -0,0 +1,13 @@
+public class IfExample {
+    public static void main(String[] args){
+        int age = 18;
+        if(age<18){
+            System.out.println("you are underage");
+        }else if(age == 18){
+            System.out.println("you just became an adult");
+        }
+        else{
+            System.out.println("you are an adult");
+        }
+    }
+}
---
+++ src/VariablesDemo.java
@@ -0,0 +1,14 @@
+public class VariablesDemo {
+    public static void main(String[] args){
+        int age = 25;
+        double height = 1.82;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Samuel";
+
+        System.out.println(name + "is" + age + "years old");
+        System.out.println("height is " + height  + "m");
+        System.out.println("grade is " + grade);
+        System.out.println("is student" + isStudent);
+    }
+}
